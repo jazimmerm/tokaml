76de5bccc1ff04be5dfd6a21d5318cf4bceb2c68
jazimmerm <jaz.zimms@gmail.com>
2021-03-22 09:42:29 -0400

    requirements.txt

--- SULI2021/random_tools/to_parquet.py
+++ SULI2021/random_tools/to_parquet.py
@@ -3,12 +3,11 @@ import os
 import pyarrow
 
 
-base = '/home/jazimmerman/PycharmProjects/SULI2021/SULI2021/data/B3/B3/'
+base = '/home/jazimmerman/PycharmProjects/SULI2021/SULI2021/data/B3/parquet'
 files = os.listdir(base)
 
 for file in files:
-    df = pd.read_csv(os.path.join(base, file), delimiter = " ")
+    df = pd.read_csv(os.path.join(base, file), delimiter=" ")
     df.columns = ['time', 'amplitude']
-    print('/home/jazimmerman/PycharmProjects/SULI2021/SULI2021/data/B3/{}.parquet'.format(file.split('.')[0]))
-
-    df.to_parquet('/home/jazimmerman/PycharmProjects/SULI2021/SULI2021/data/B3/parquet/{}.pq'.format(file.split('.')[0]), engine='pyarrow')
+    print(f'/home/jazimmerman/PycharmProjects/SULI2021/SULI2021/data/B3/{file.split(".")[0]}.pq')
+    df.to_parquet(f'/home/jazimmerman/PycharmProjects/SULI2021/SULI2021/data/B3/parquet/{file.split(".")[0]}.pq', engine='pyarrow')
